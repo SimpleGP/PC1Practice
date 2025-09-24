dd7704ae6085ba7839b9840f60f6aaad308148e1
SimpleGP <giulianopv2@gmail.com>
2025-09-24 01:47:19 -0500

    aea

---
+++ src/components/footer.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer class="text-center p-4 mt-4 border-t">
+    <p>Copyright © 2024 Fake TV Shows API, inc. All rights reserved.</p>
+    <p>Developer by u20221e121 -Giuliano Pelaez</p>
+  </footer>
+</template>
+<script>
+/*
+ *@summary Footer with copyright and developer info
+ */
+export default {
+  name:'app-footer'
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { fetchShows } from '../services/show.js'
+import ShowCard from '../components/show-card.vue'
+import AppToolbar from '../components/toolbar.vue'
+import AppFooter from '../components/footer.vue'
+
+const shows = ref([])
+
+onMounted(async () => {
+  shows.value = await fetchShows()
+})
+</script>
+
+<template>
+  <div>
+
+
+    <section class="p-4">
+      <h2 class="text-2xl mb-4">All Shows</h2>
+      <div class="flex flex-wrap justify-content-center">
+        <ShowCard v-for="(s, i) in shows" :key="s.name" :show="s" :index="i" />
+      </div>
+    </section>
+
+
+  </div>
+</template>
